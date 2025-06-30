14c04ca6cbc2e8fa8e9657ca769fa4f95bc05c34
Jasper Drescher <jasperdre@gmail.com>
2025-06-30 18:09:39 +0900

    Add WIP character controller

---
+++ Farm/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class CharacterController : MonoBehaviour
+{
+    public float rotationSpeed = 10.0f;
+    public float moveSpeed = 10.0f;
+
+    private Animator animator;
+    private Vector3 moveDirection;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveDirection.magnitude != 0.0f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            Debug.Log(playerRotation);
+            transform.rotation = playerRotation;
+            var translation = moveDirection * (moveSpeed * Time.deltaTime);
+            transform.Translate(translation);
+        }
+    }
+
+    public void InputMove(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.canceled)
+        {
+            animator.SetBool("IsWalking", false);
+            moveDirection = Vector3.zero;
+            return;
+        }
+
+        animator.SetBool("IsWalking", true);
+        Vector2 input = callbackContext.ReadValue<Vector2>();
+        Debug.Log(input);
+        moveDirection = new Vector3(input.x, 0.0f, input.y);
+    }
+}
