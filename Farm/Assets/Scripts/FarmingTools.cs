ad56b1ad8ff6e9af75cb08abdde3ea2e5acf22df
hcodename47 <hcodename47@gmail.com>
2025-07-02 15:25:26 +0900

    Added farming tools enum

---
+++ Farm/Assets/Scripts/FarmingTools.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FarmingTools : MonoBehaviour
+{
+	enum EFarmingTools
+	{ 
+		None,
+		Rake,
+		Spade,
+		WaterBucket,
+		Sickle,
+	}
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
