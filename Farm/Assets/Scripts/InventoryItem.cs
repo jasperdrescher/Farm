4d1d8b4b5a84bcf52bda5c8192c0274cd9c648b7
hcodename47 <hcodename47@gmail.com>
2025-07-11 17:59:10 +0900

    Item stacking support in InventoryItem

--- Farm/Assets/Scripts/InventoryItem.cs
+++ Farm/Assets/Scripts/InventoryItem.cs
@@ -22,11 +22,37 @@ public class InventoryItem : ScriptableObject
 	public GameObject m_gameObjectPrefab;
 
 	public bool m_hideInInventory = false;
+	public bool m_stackable = true;
+
+	// todo: max stack size?
 
 	public Sprite m_thumbnail;
 
 	public bool IsVisibleInInventory()
 	{
 		return !m_hideInInventory && m_itemType != Type.Currency && m_itemType != Type.PlayerResource;
+	}
+
+	public bool IsStackable()
+	{ 
+		return m_stackable;
+	}
+
+	public bool IsSameItem(InventoryItem otherItem)
+	{ 
+		return m_itemType == otherItem.m_itemType && m_cropType == otherItem.m_cropType && m_resourceType == otherItem.m_resourceType;
+	}
+
+	public bool CanStackWith(InventoryItem otherItem)
+	{ 
+		return IsStackable() && IsSameItem(otherItem);
+	}
+
+	public void StackItem(InventoryItem otherItem)
+	{
+		if (!CanStackWith(otherItem))
+			return;
+
+		m_amount += otherItem.m_amount;
 	}
 }
